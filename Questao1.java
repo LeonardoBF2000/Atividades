fe6243524d3a0ea92f86c5baa2fc38daeb8c0114
LeonardoBF2000 <98906993+LeonardoBF2000@users.noreply.github.com>
2022-02-02 14:42:02 -0300

    Create Questao1.java

---
+++ Questao1.java
@@ -0,0 +1,14 @@
+package Questao1;
+public class Questao1 {
+
+
+    public static void main(String[] args) {
+    int SOMA = 0,K = 0,INDICE = 13; ;
+while(K<INDICE){
+K = K + 1;
+SOMA = SOMA + K;
+}
+System.out.println(SOMA);
+    }
+    
+}
